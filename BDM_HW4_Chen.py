4362843cafd15e7fd70496f552c11e9523c53f46
Xin Chen <chenx23333@gmail.com>
2021-12-04 13:28:25 -0500

    Update BDM_HW4_Chen.py

--- BDM_HW4_Chen.py
+++ BDM_HW4_Chen.py
@@ -1,39 +1,48 @@
 import sys
 import csv
 import datetime
-import json
 import pyspark
 import numpy as np
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 from datetime import date
+import itertools
 
-def map_partition(y):
-  for x in y:
-    xx = x.split(',')
-    yield (xx[0],xx[1])
 
-def get_date(y):
-  for x in y:
-    yield (x[12].split('T')[0],x[13].split('T')[0],x[16])
-  
+def get_name_id(x):
+    for data in categories:
+      if x[9] in data:
+        return [x[0],x[1],data[-1]]
+
+def get_pattern_info(x):
+  if x[0] in data:
+    return (x[12].split('T')[0],x[13].split('T')[0],x[16],x[0],x[1])
 
 def visit_per_day(visit_data):
-  
-  d = visit_data[0].split('-')
-  d0 = date(int(d[0]),int(d[1]),int(d[2]))
-  d = visit_data[1].split('-')
-  d1 = date(int(d[0]),int(d[1]),int(d[2]))
-  diff = d1 - d0
-  delta = datetime.timedelta(days=1)
-  visit = visit_data[2].replace('[','').replace(']','')
-  for x in visit.split(','):
-    yield (str(d0),int(x))
-    d0+= delta
+  if visit_data is not None:
+    d = visit_data[0].split('-')
+    d0 = date(int(d[0]),int(d[1]),int(d[2]))
+    d = visit_data[1].split('-')
+    d1 = date(int(d[0]),int(d[1]),int(d[2]))
+    diff = d1 - d0
+    delta = datetime.timedelta(days=1)
+    visit = visit_data[2].replace('[','').replace(']','')
+    for x in visit.split(','):
+      yield (  '_'.join( (str(d0), visit_data[3],visit_data[4])),int(x))
+      d0+= delta
 
 def get_table_format(data):
   for x in data:
-    yield (x[0].split('-')[0],x[0].replace('2019','2020'),int(np.median(x[1])),list(count_low(x))[0],list(count_high(x))[0]  )
+    y = x[0].split('_')
+    yield (
+           info_dic[(y[1],y[2])], 
+           (
+           y[0].split('-')[0],y[0].replace('2019','2020'),
+           int(np.median(x[1])),
+           list(count_low(x))[0],
+           list(count_high(x))[0]
+           )
+           )
 
 def count_low(x):
   low = round(int(np.median(x[1])) - int(np.std(x[1])))
@@ -49,46 +58,79 @@ def count_high(x):
   else:
     yield high
 
+def to_list(a):
+    return [a]
+
+def append(a, b):
+    a.append(b)
+    return a
+
+def extend(a, b):
+    a.extend(b)
+    return a
   
 if __name__=='__main__':
   sc = pyspark.SparkContext()
   spark = SparkSession(sc)
+  categories = [   
+      ['452210','452311','big_box_grocers'],
+      ['445120','convenience_stores'],
+      ['722410','drinking_places'],
+      ['722511','full_service_restaurants'],
+      ['722513','limited_service_restaurants'],
+      ['446110','446191','pharmacies_and_drug_stores'],
+      ['311811','722515','snack_and_bakeries'],
+      ['445210','445220','445230','445291','445292','445299','specialty_food_stores'],
+      ['445110','supermarkets_except_convenience_stores'] ]
 
-  categories = [set(['452210','452311']),set(['445120']),set(['722410']),set(['722511']),
-              set(['722513']), set(['446110','446191']),set(['311811','722515']),
-                set( ['445210','445220','445230','445291','445292','445299']), 
-                set(['445110'])]
-  path_name = ('test/big_box_grocers',
-            'test/convenience_stores',
-            'test/drinking_places',
-            'test/full_service_restaurants',
-            'test/limited_service_restaurants',
-            'test/pharmacies_and_drug_stores',
-            'test/snack_and_bakeries',
-            'test/specialty_food_stores',
-            'test/supermarkets_except_convenience_stores')
+  id = ['452210','452311','445120','722410','722511','722513','446110','446191','311811','722515',
+        '445210','445220','445230','445291','445292','445299','445110']
 
-  for index,categorie in enumerate(categories):
+  path_name = {
+            'big_box_grocers':'test/big_box_grocers',
+            'convenience_stores':'test/convenience_stores',
+            'drinking_places':'test/drinking_places',
+            'full_service_restaurants':'test/full_service_restaurants',
+            'limited_service_restaurants':'test/limited_service_restaurants',
+            'pharmacies_and_drug_stores':'test/pharmacies_and_drug_stores',
+            'snack_and_bakeries': 'test/snack_and_bakeries',
+            'specialty_food_stores':'test/specialty_food_stores',
+            'supermarkets_except_convenience_stores':'test/supermarkets_except_convenience_stores'}
+                  
+  
     # hdfs:///data/share/bdm/core-places-nyc.csv
-    get_info = sc.textFile('hdfs:///data/share/bdm/core-places-nyc.csv')\
-                .filter(lambda x: next(csv.reader([x]))[9] in categorie)\
-                .mapPartitions(map_partition)\
-                .cache()\
-                .collect()
-                
+  get_info = sc.textFile('hdfs:///data/share/bdm/core-places-nyc.csv')\
+              .map(lambda  x: get_name_id(next(csv.reader([x])))  )\
+              .filter(lambda x: x is not None)\
+              .cache()\
+              .collect()
+  data = list(itertools.chain(*get_info))
+  info_dic = {}
+  for y in get_info:
+    y = ((y[0],y[1]),y[2])
+    d = dict(itertools.zip_longest(*[iter(y)] * 2, fillvalue=""))
+    key = list(d.keys())[0]
+    info_dic[key] = d[key]
+  
 
 
+#   hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/*
+  rdd = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/*')\
+          .map(lambda x: get_pattern_info(next(csv.reader([x])) ))\
+          .filter(lambda x: x is not None)\
+          .flatMap(visit_per_day)\
+          .filter(lambda x: x[1] != 0 and x[0].split('_')[0] >'2018-12-31' and x[0].split('_')[0] < '2021-01-01')\
+          .combineByKey(to_list, append , extend)\
+          .sortBy(lambda x: x[0])\
+          .mapPartitions(get_table_format)\
+          .combineByKey(to_list, append , extend)\
+          .collect()
 
-    get_visit_data = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/*')\
-                      .filter(lambda x: tuple(next(csv.reader([x]))[0:2]) in  get_info )\
-                      .map(lambda x: next(csv.reader([x])))\
-                      .mapPartitions(get_date)\
-                      .flatMap(visit_per_day)\
-                      .filter(lambda x: x[1] != 0 and x[0] >'2018-12-31' and x[0] < '2021-01-01')\
-                      .combineByKey((lambda lst: [lst]), (lambda sum_lst, item: sum_lst + [item]), (lambda sum_lst1, sum_lst2: sum_lst1 + sum_lst2))\
-                      .sortBy(lambda x: x[0])\
-                      .mapPartitions(get_table_format)\
-                      .toDF(["year", "date" , "median","low","high"])\
-                      .write.format("csv")\
-                      .option("header", "true")\
-                      .save(path_name[index])
+  for categorie in rdd:
+          spark.coalesce(1)\
+               .createDataFrame(categorie[1], ["year", "date" , "median","low","high"])\
+               .write.format("csv")\
+               .option("header", "true")\
+               .save(path_name[categorie[0]])
+
+
