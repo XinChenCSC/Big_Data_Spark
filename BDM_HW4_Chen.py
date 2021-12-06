dc791e622058a33c6f7227c258fbb6d868552854
Xin Chen <chenx23333@gmail.com>
2021-12-05 22:35:09 -0500

    Update BDM_HW4_Chen.py

--- BDM_HW4_Chen.py
+++ BDM_HW4_Chen.py
@@ -98,32 +98,23 @@ if __name__=='__main__':
                     
 
 
-  res = visit_info.join(id_info,( (visit_info.placekey == id_info.id_placekey) & (visit_info.safegraph_place_id == id_info.id_safegraph_place_id)  ),"inner" )\
-                  .drop('parent_placekey')\
-                  .drop('parent_safegraph_place_id')\
+  res = visit_info.join(id_info, on = "safegraph_place_id"  ) \
                   .dropna()\
-                   .select('name',
-                           F.explode(udf_visit_per_day('date_range_start','date_range_end','visits_by_day'))\
-                            .alias('date','visits'))\
-                   .groupBy('name','date')\
-                   .agg(F.collect_list('visits').alias('visits'))\
-                   .sort('date')\
-                   .withColumn('median',udf_count_median('visits') )\
-                   .withColumn('low',udf_count_low('visits') )\
-                   .withColumn('high',udf_count_high('visits') )\
-                   .drop('visits')\
-                   .withColumn("year",F.split(F.col('date'),'-')[0]   )\
-                   .withColumn("date",udf_get_year(F.col('date')) )
+                  .select('name',
+                          F.explode(udf_visit_per_day('date_range_start','date_range_end','visits_by_day'))\
+                          .alias('date','visits'))\
+                  .groupBy('name','date')\
+                  .agg(F.collect_list('visits').alias('visits'))\
+                  .sort('date')\
+                  .withColumn('median',udf_count_median('visits') )\
+                  .withColumn('low',udf_count_low('visits') )\
+                  .withColumn('high',udf_count_high('visits') )\
+                  .drop('visits')\
+                  .withColumn("year",F.split(F.col('date'),'-')[0]   )\
+                  .withColumn("date",udf_get_year(F.col('date')) )
 
   for key,value in path_name.items():
     res.where(F.col('name') == key)\
         .select('year','date','median','low','high')\
         .coalesce(1)\
-        .write.format("csv")\
-        .option("header", "true")\
-        .save(value)
-#         .save(value.replace('test',sys.argv[1]) if len(sys.argv)>1 else value)
-#         .save(value)
-
-
-    
+        .write.csv(value)
